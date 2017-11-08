12d32352d7401d193cffa90e2ecc7743d5e48a25
raviteja548 <raviteja.m548@gmail.com>
2017-11-08 21:48:43 +0530

    merged

--- PowRecursive.java
+++ PowRecursive.java
@@ -1,11 +1,7 @@
 package questions;
 //PowRecursive
 public class PowRecursive {
-<<<<<<< .mine
 	// main method execution
-=======
-	//main method
->>>>>>> .theirs
 	public static void main(String[] args) {
 		int n = PowRecursive.pow(2, 6);
 		System.out.println(n);
